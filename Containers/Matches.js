3d0f7e7fb41415c2cb34181262cb9c715bee9a8a
Achiaga <alfonso.achiaga@gmail.com>
2019-09-08 16:51:03 +0200

    Structure Page

--- Containers/Matches.js
+++ Containers/Matches.js
@@ -1,16 +1,100 @@
 import React, { Component } from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import {
+	Text,
+	View,
+	Button,
+	Image,
+	StyleSheet,
+	ScrollView,
+} from 'react-native';
 
 const Matches = props => {
 	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-			<Text>Matches</Text>
-			<Button
-				title='Messages'
-				onPress={() => {
-					props.navigation.navigate({ routeName: 'Messages' });
-				}}
-			/>
+		<View
+			style={{
+				marginTop: 50,
+				marginBottom: 50,
+			}}>
+			<ScrollView>
+				<View
+					style={{
+						flex: 1,
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Text>Matches</Text>
+					<View
+						style={{
+							backgroundColor: 'gray',
+						}}>
+						<View
+							style={{
+								flexDirection: 'row',
+								flexWrap: 'wrap',
+								marginHorizontal: 30,
+							}}>
+							<Image
+								source={{
+									uri:
+										'https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half-caption/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=0hb44OrI',
+								}}
+								style={{ width: 150, height: 150 }}
+							/>
+							<Image
+								source={{
+									uri:
+										'https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg?quality=98&strip=all&w=782',
+								}}
+								style={{ width: 150, height: 150 }}
+							/>
+						</View>
+						<Text>Maria</Text>
+					</View>
+					<View
+						style={{
+							border: '1px solid black',
+						}}>
+						<View
+							style={{
+								flexDirection: 'row',
+								flexWrap: 'wrap',
+								marginHorizontal: 30,
+							}}>
+							<Image
+								source={{
+									uri:
+										'http://dev.nrt24.ru/wp-content/uploads/2017/08/Mila-Kunis.jpg',
+								}}
+								style={{ width: 150, height: 150 }}
+							/>
+							<Image
+								source={{
+									uri:
+										'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/bernese-mountain-dog-royalty-free-image-487880819-1565106514.jpg',
+								}}
+								style={{ width: 150, height: 150 }}
+							/>
+						</View>
+						<Text>Juana</Text>
+					</View>
+					<View>
+						<Image
+							source={{
+								uri:
+									'https://listaka.com/wp-content/uploads/2017/12/shutterstock_530967340-1.jpg',
+							}}
+							style={{ width: 200, height: 200 }}
+						/>
+						<Text>Rex</Text>
+					</View>
+					<Button
+						title='Messages'
+						onPress={() => {
+							props.navigation.navigate({ routeName: 'Messages' });
+						}}
+					/>
+				</View>
+			</ScrollView>
 		</View>
 	);
 };
--- Containers/Messages.js
+++ Containers/Messages.js
@@ -1,31 +1,70 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, ScrollView } from 'react-native';
 
 const Messages = props => {
 	return (
-		<View style={styles.screen}>
-			<Text>Messages</Text>
-			<Button
-				title='Blog'
-				onPress={() => {
-					props.navigation.navigate({ routeName: 'Blog' });
-				}}
-			/>
-			<Button
-				title='Go Back'
-				onPress={() => {
-					props.navigation.goBack();
-				}}
-			/>
+		<View style={styles.container}>
+			<ScrollView>
+				<View style={styles.item1}>
+					<Text style={{ fontSize: 20, color: '#fff' }}>Item number 1</Text>
+				</View>
+				<View style={styles.item2}>
+					<Text style={{ fontSize: 20, color: '#fff' }}>Item number 1</Text>
+				</View>
+				<View style={styles.item3}>
+					<Text style={{ fontSize: 20, color: '#fff' }}>Item number 1</Text>
+				</View>
+				<View style={styles.item4}>
+					<Text style={{ fontSize: 20, color: '#fff' }}>Item number 1</Text>
+				</View>
+				<Button
+					title='Blog'
+					onPress={() => {
+						props.navigation.navigate({ routeName: 'Blog' });
+					}}
+				/>
+				<Button
+					title='Go Back'
+					onPress={() => {
+						props.navigation.goBack();
+					}}
+				/>
+			</ScrollView>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	screen: {
+	container: {
 		flex: 1,
+		marginTop: 40,
 		justifyContent: 'center',
-		alignItems: 'center',
+		backgroundColor: '#fff',
+		alignItems: 'stretch',
+	},
+	title: {
+		fontSize: 20,
+		color: '#fff',
+	},
+	item1: {
+		backgroundColor: 'orange',
+		flex: 1,
+		height: 120,
+	},
+	item2: {
+		backgroundColor: 'purple',
+		flex: 1,
+		height: 120,
+	},
+	item3: {
+		backgroundColor: 'yellow',
+		flex: 1,
+		height: 120,
+	},
+	item4: {
+		backgroundColor: 'red',
+		flex: 1,
+		height: 120,
 	},
 });
 
